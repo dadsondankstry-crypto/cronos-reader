ee3df426590f6597e5f18e0f7c4c0fbd565af26d
dadsondankstry-crypto <dadsondankstry@gmail.com>
2026-01-29 11:04:34 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,15 @@
-const CACHE = "cronos-final-v1";
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["./", "index.html", "style.css", "app.js", "manifest.json"])));
+const CACHE_NAME = 'cronos-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
